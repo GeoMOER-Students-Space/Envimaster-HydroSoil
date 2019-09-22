acca8e69fd3d37f59616878a0d011c5f95ab3d5e
SchoenbergA <Schoenb4@students.uni-marburg.de>
2019-09-22 14:03:00 +0200

    init repo

---
+++ src/002_head_hydro_withSAGA_v1.R
@@ -0,0 +1,24 @@
+#############################################################################################
+###--- Setup Environment -------------------------------------------------------------------#
+                                  ###############################################           #
+# require libs for setup          #EEEE n   n v       v rrrr    m     m   ttttt #           #                  
+require(raster)                   #E    nn  n  v     v  r   r  m m   m m    t   #           #         
+require(envimaR)                  #EE   n n n   v   v   rrrr   m m   m m    t   #           #                
+require(link2GI)                  #E    n  nn    v v    r  r  m   m m   m   t   #           #             
+                                  #EEEE n   n     v     r   r m    m    m   t   #           #
+                                  ###############################################           #
+                                                                                            #
+# define needed libs and src folder                                                         #
+libs = c("link2GI") 
+pathdir = "src"
+
+#set root folder for uniPC or laptop                                                        #
+root_folder = alternativeEnvi(root_folder = "~/edu/Envimaster-HydroSoil",                    #
+                              alt_env_id = "COMPUTERNAME",                                  #
+                              alt_env_value = "PCRZP",                                      #
+                              alt_env_root_folder = "F:/edu/Envimaster-HydroSoil")          #
+#source environment script                                                                  #
+source(file.path(root_folder, paste0(pathdir,"/001_setup_hydro_withSAGA_v1.R")))                                                               
+###---------------------------------------------------------------------------------------###
+#############################################################################################
+
